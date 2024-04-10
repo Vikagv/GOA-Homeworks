3de773137a476c7089decf99b8c28583565da1c5
Vikagv <vikagvilia@Yahoo.com>
2024-04-10 18:22:03 +0100

    day 20

---
+++ Day20/homework20.py
@@ -0,0 +1,9 @@
+#print,Variables,int,string,float,input,true or false.
+#i in range.
+
+for i in range(100):
+    print("მიზანზე ორიენტირებული აკადემია საუკეთესოა!")
+
+for i in range(10,200,10):
+    print(i)
+    print(str(i) + "This number can be divided by 10")
