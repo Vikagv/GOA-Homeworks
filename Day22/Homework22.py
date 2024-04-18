408c1b16fb6ac9b1b777db00142c3d0a75631981
Vikagv <vikagvilia@Yahoo.com>
2024-04-18 16:05:09 +0100

    Day22

---
+++ Day22/Homework22.py
@@ -0,0 +1,5 @@
+Best_country=input ("What is the best country?:  ")
+
+while Best_country!= "GEORGIA!" :
+    Best_country= input ("What is the best country?")
+
---
+++ Day22/claswork22.py
@@ -0,0 +1,6 @@
+Best_acadamy=input ("What is the best acadamy in georgia?:  ")
+
+while Best_acadamy!= "goal_oriented_acadamy" :
+    Best_acadamy= input ("What is the best acadamy in georgia?")
+
+print(goa=Best_acadamy)
