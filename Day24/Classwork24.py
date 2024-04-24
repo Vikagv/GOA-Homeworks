400788bc4fcc44c343cf2d20356ed10be004f8fd
Vikagv <vikagvilia@Yahoo.com>
2024-04-24 17:42:59 +0100

    Day24

---
+++ Day24/Classwork24.py
@@ -0,0 +1,14 @@
+user_number=int (input ("Please insert a number of your choice:"))
+
+while user_number % 5:
+   user_number= int (input ("Please insert a number of your choice:"))
+print("well done")
+
+
+# second classwork
+
+Password=input ("what is the password?:  ")
+
+while Password!= "GOA_IS_THE_BEST" :
+    Password= input ("what is the password?")
+
---
+++ Day24/Homework24.py
@@ -0,0 +1,6 @@
+user_number=int (input ("Please insert a number of your choice:"))
+
+while user_number % 100:
+   user_number= int (input ("Please insert a number of your choice:"))
+print("YOU PICKED THE CORRECT NUMBER!! WELL DONE!!")
+
