c5f7e075860c58ee2d5d61fadac9e7f7652f6798
Vikagv <vikagvilia@Yahoo.com>
2024-05-01 19:35:31 +0100

    Day26

---
+++ Day26/Homework26.py
@@ -0,0 +1,14 @@
+GOA= (input ("Do you study in Goal_oriented_acadamy?"))
+while GOA!=("Yes"):
+     GOA= input ("Try Again!:")
+
+GOA2= (input ("Thats great,What group do you study in?"))
+while GOA2!=("Group 13"):
+     print ("Nice have a good time!")
+
+GOA3= (input ("Is Gabriel your mentor?:  "))
+while GOA3!=("Yes"):
+    print("You ARNT in group 13, You lied!!")
+
+print ("Congrats! you were telling the truth!")
+
