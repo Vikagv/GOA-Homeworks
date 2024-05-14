55830e1084f134e15f482f2c79a19e6d3e6df58e
Vikagv <vikagvilia@Yahoo.com>
2024-05-14 20:08:01 +0100

    Day30

---
+++ Day30/Homework30.py
@@ -0,0 +1,10 @@
+# Create a list in which the names of your favorite car will be, one enter 
+#  something that you do not like, then change
+#  this item with the name of another car, print all the items
+
+
+Cars=["Mercedes-benz","BMW","Honda","Audi"]
+
+Cars[2]="Volvo"
+
+print (Cars)
