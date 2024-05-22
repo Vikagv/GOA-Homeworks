5ee02d2e6a79beb79abaf1c2d68e563763160e16
Vikagv <vikagvilia@Yahoo.com>
2024-05-22 18:17:45 +0100

    commit

---
+++ Day32/Homework/Homework.py
@@ -0,0 +1,12 @@
+#. Like today's lesson, change "goa is the best"
+#. through the for loop and with the indexing learned
+#. today, connect "goa is the best" without spaces ❤️
+
+Academy = "GoaistheBest"
+
+new_string = ""
+
+for i in range (12):
+    new_string += Academy [i]
+
+print (new_string)
