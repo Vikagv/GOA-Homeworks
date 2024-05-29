befab85f5bc05f75001bc74f23891ada344c9a9c
Vikagv <vikagvilia@Yahoo.com>
2024-05-29 12:19:59 +0100

    Day34

---
+++ Day34/Homework/Homework.py
@@ -0,0 +1,10 @@
+#. 3) Create a list to which you pass all the letters of the 
+#. alphabet, then you need to find g, o, and a in this list, 
+#. concatenate these three letters and print goa
+
+Alphabet = ["A","B", "C", "D", "E", "F", "G", "H", "I", "J",
+"K", "L", "M", "N", "O", "P", "Q" "R", 
+"S","T", "U", "V", "W", "X", "Y", "Z" ]
+print(Alphabet[6])
+print(Alphabet[14])
+print(Alphabet[0])
---
+++ Day34/Homework/Homework34.py
@@ -0,0 +1,38 @@
+#.2) შექმნათ სია სადაც ჩაამატებთ 
+#.თქვენი ყველა ჯგუფელის სახელს და გვარს,
+#. შემდეგ კი დაბეჭდავთ შუა 10 მოსწავლეს.
+
+group13_members = ["Aleko Tirkia","Anastasia Khvedelidze",
+"Andria Alavidze","Andria Iakobishvili","Andria Korchilava",
+"Andria Samsonidze","Andria Shanava","Barbare Tinikashvili",
+"Beridze Mate","Dachi Sazandrishvili","Data Tezelashvili",
+"Dato Berkacashvili","Dato Miribiani","Davit Grdzelishvili",
+"Davit Janashia","Deme Biganashvili","Erekle Karagozishvili",
+"Gabriel Gvinashvili","Gabrieli Molodini","Gio Alada",
+"Gio Gagnidze","Giorgi Vanishvili","Giviko Kavrishvili",
+"Guram Vakhtangashvili","Guri Jishkariani","Ilia Wiklauri",
+"Lasha Lomidze",
+"Leqso Leverashvili","Luka Chakhvashvili","Luka Luka",
+"Luka Natroshvili",
+"Luka Navrozashvili","Luka Tskhvaradze","Luxumi Zurabiani",
+"Masho Lo","Mate Giorgelashvili","Mate Kotorashvili",
+"Mate Rekhviashvili",
+"Mate Tolordava","Mekaluashvili Andria","Nika Chunashvili",
+"Nika Nachhkepia",
+"Nika Shavadze","Nikolas Ndiaye","Nikoloz Filishvili",
+"Nikoloz Nutsubidze",
+"Nikolozi Qimeridze","Qristine Kiknadze","Saba Kublashvili",
+"Saba Lomsadze","Sandro Chapidze","Sandro Zabakhidze",
+"Tazo Kldiashvili",
+"Toma Omanashvili","Vano Motiashvili","Vika Gvilia",
+"Zura Beridze",
+"ალექსანდრე სოხაძე","ბიბილაშვილი გიორგი","გეგა დიასამიძე",
+"გიორგი ქარელი",
+"დავით გიგილაშვილი","დათა დიასამიძე","დათო ტყეშელაშვილი",
+"დათუნა ციხელაშვილი","ზარდიაშვილი მარიამ","მიშიკო ბერელიძე",
+"ნიკო კეჭეყმაძე","ნიკოლოზ კუკავა","ნიკოლოზ მირზიაშვილი",
+"ნიკოლოზ ნინუა",
+"ნიკუშა კუბლაშვილი","საბა ნაკაშძე"]
+
+print(group13_members[31:41])
+
