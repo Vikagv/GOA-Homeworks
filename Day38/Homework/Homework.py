9e92bcf81abf0f66d2f0df7f980d513839444970
Vikagv <vikagvilia@Yahoo.com>
2024-06-12 15:16:27 +0100

    Day38

---
+++ Day38/Homework/Homework.py
@@ -0,0 +1,13 @@
+movies=["Little Man","grown Ups1","Grown ups2","White Chicks","Mask","MIB","Dumb and Dumber"
+,"the Karate kid","young sheldon","Jumanji"]
+
+print(movies[0])
+print(movies[1])
+print(movies[2])
+print(movies[3])
+print(movies[4])
+print(movies[5])
+print(movies[6])
+print(movies[7])
+print(movies[8])
+print(movies[9])
---
+++ Day38/Homework/Homework2.py
@@ -0,0 +1,12 @@
+Family=["Ana","Nika","Mari","Nini","Vika","Emily","Taso","Kato","Mako","Giorgi"]
+
+print(Family[0])
+print(Family[1])
+print(Family[2])
+print(Family[3])
+print(Family[4])
+print(Family[5])
+print(Family[6])
+print(Family[7])
+print(Family[8])
+print(Family[9])
