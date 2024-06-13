f6191ea4b25083e34fc640510732908883464fe5
Vikagv <vikagvilia@Yahoo.com>
2024-06-13 18:06:59 +0100

    Day38

---
+++ Day38/Homework/Homework3.py
@@ -0,0 +1,109 @@
+group13_members = ["Aleko Tirkia","Anastasia Khvedelidze",
+"Andria Alavidze","Andria Iakobishvili","Andria Korchilava",
+"Andria Samsonidze","Andria Shanava","Barbare Tinikashvili",
+"Beridze Mate","Dachi Sazandrishvili","Data Tezelashvili",
+"Dato Berkacashvili","Dato Miribiani","Davit Grdzelishvili",
+"Davit Janashia","Deme Biganashvili","Erekle Karagozishvili",
+"Gabriel Gvinashvili","Gabrieli Molodini","Gio Alada",
+"Gio Gagnidze","Giorgi Vanishvili","Giviko Kavrishvili",
+"Guram Vakhtangashvili","Guri Jishkariani","Ilia Wiklauri",
+"Lasha Lomidze",
+"Leqso Leverashvili","Luka Chakhvashvili","Luka Luka",
+"Luka Natroshvili",
+"Luka Navrozashvili","Luka Tskhvaradze","Luxumi Zurabiani",
+"Masho Lo","Mate Giorgelashvili","Mate Kotorashvili",
+"Mate Rekhviashvili",
+"Mate Tolordava","Mekaluashvili Andria","Nika Chunashvili",
+"Nika Nachhkepia",
+"Nika Shavadze","Nikolas Ndiaye","Nikoloz Filishvili",
+"Nikoloz Nutsubidze",
+"Nikolozi Qimeridze","Qristine Kiknadze","Saba Kublashvili",
+"Saba Lomsadze","Sandro Chapidze","Sandro Zabakhidze",
+"Tazo Kldiashvili",
+"Toma Omanashvili","Vano Motiashvili","Vika Gvilia",
+"Zura Beridze",
+"ალექსანდრე სოხაძე","ბიბილაშვილი გიორგი","გეგა დიასამიძე",
+"გიორგი ქარელი",
+"დავით გიგილაშვილი","დათა დიასამიძე","დათო ტყეშელაშვილი",
+"დათუნა ციხელაშვილი","ზარდიაშვილი მარიამ","მიშიკო ბერელიძე",
+"ნიკო კეჭეყმაძე","ნიკოლოზ კუკავა","ნიკოლოზ მირზიაშვილი",
+"ნიკოლოზ ნინუა",
+"ნიკუშა კუბლაშვილი","საბა ნაკაშძე"]
+
+
+print(group13_members[0])
+print(group13_members[1])
+print(group13_members[2])
+print(group13_members[3])
+print(group13_members[4])
+print(group13_members[5])
+print(group13_members[6])
+print(group13_members[7])
+print(group13_members[8])
+print(group13_members[9])
+print(group13_members[10])
+print(group13_members[11])
+print(group13_members[12])
+print(group13_members[13])
+print(group13_members[14])
+print(group13_members[15])
+print(group13_members[16])
+print(group13_members[17])
+print(group13_members[18])
+print(group13_members[19])
+print(group13_members[20])
+print(group13_members[21])
+print(group13_members[22])
+print(group13_members[23])
+print(group13_members[24])
+print(group13_members[25])
+print(group13_members[26])
+print(group13_members[27])
+print(group13_members[28])
+print(group13_members[29])
+print(group13_members[30])
+print(group13_members[31])
+print(group13_members[32])
+print(group13_members[33])
+print(group13_members[34])
+print(group13_members[35])
+print(group13_members[36])
+print(group13_members[37])
+print(group13_members[38])
+print(group13_members[39])
+print(group13_members[40])
+print(group13_members[41])
+print(group13_members[42])
+print(group13_members[43])
+print(group13_members[44])
+print(group13_members[45])
+print(group13_members[46])
+print(group13_members[47])
+print(group13_members[48])
+print(group13_members[49])
+print(group13_members[50])
+print(group13_members[51])
+print(group13_members[52])
+print(group13_members[53])
+print(group13_members[54])
+print(group13_members[55])
+print(group13_members[56])
+print(group13_members[57])
+print(group13_members[58])
+print(group13_members[59])
+print(group13_members[60])
+print(group13_members[61])
+print(group13_members[62])
+print(group13_members[63])
+print(group13_members[64])
+print(group13_members[65])
+print(group13_members[66])
+print(group13_members[67])
+print(group13_members[68])
+print(group13_members[69])
+print(group13_members[70])
+print(group13_members[71])
+print(group13_members[72])
+
+
+
