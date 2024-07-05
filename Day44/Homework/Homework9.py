5b6acd7a6f212bdcdd2b7ef3db0c419fdce787cc
Vikagv <vikagvilia@Yahoo.com>
2024-07-05 16:38:09 +0100

    Day44

--- Day44/Homework/Homework9.py
+++ Day44/Homework/Homework9.py
@@ -2,4 +2,4 @@ Temp = int(input("Enter your Temperature:"))
 if Temp < 30 :
     print ("It is not too hot outside")
 else :
-    print ("It is too hot outside")
+    print ("It is too hot outside")  
