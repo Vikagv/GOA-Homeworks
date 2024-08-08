646000e380a231b68a91538ad1bd329bed7e63b9
Vika Gvilia <vikagvilia@Yahoo.com>
2024-08-08 15:47:08 +0400

    Day54

---
+++ Day54/Homework/Homework.py
@@ -0,0 +1,8 @@
+#.Create a function that will input a user number and add 1 to it.
+
+def Caulculator():
+    User_Number = int(input("Please enter a number:"))
+    User_Sum = (User_Number + int (1)) 
+    print (User_Sum)
+
+Caulculator()
---
+++ Day54/Homework/Homework2.py
@@ -0,0 +1,8 @@
+#.Create a function that will input a number to the customer and return "Cool!" if the number is greater than 10.
+
+def Users_Number():
+    user_input = int(input("Enter any Number"))
+    if user_input > 10 :
+        print("Cool!")
+
+User_Number()
