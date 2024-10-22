a063d2a9cc4ff3ead5659be34ad1c6b00d558732
Vikagv <vikagvilia@Yahoo.com>
2024-10-22 17:08:57 +0100

    Day71

---
+++ Day71/Homework/Homework.js
@@ -0,0 +1,10 @@
+//dynamic array in javascript//
+
+let dyArray = [10, 20 ,30];
+
+dyArray.push(40);
+dyArray.push(50);
+
+dyArray.pop();
+
+console.log(dyArray);
---
+++ Day71/Homework/Homework2.js
@@ -0,0 +1,7 @@
+//4) Write a function that generates a random number between 1 and 100 using Math.random(). Ensure the number is an integer.//
+
+function random_num() {
+    return Math.floor(math.random() * 100) + 1;
+}
+const random_number = random_num();
+console.log(random_number);
