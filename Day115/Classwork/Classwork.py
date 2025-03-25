02ec9f366b998a927391f2248d793938bea094dc
vika gvilia <vikagvilia@Yahoo.com>
2025-03-25 22:26:37 +0000

    Day118

---
+++ Day115/Classwork/Classwork.py
@@ -0,0 +1,13 @@
+#Have the user enter a number, then create a function that takes the user's entered number as an argument, and this function should return only even numbers from one to the user's number.
+
+
+Number = int(input("Enter any number"))
+
+def Odd_or_eaven():
+    if Number % 2:
+     print ("your Number is Even")
+
+
+
+
+     
