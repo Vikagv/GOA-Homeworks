ec2893ea8a7c613d157643e32ac88107762079e9
vika gvilia <vikagvilia@Yahoo.com>
2025-05-16 17:16:30 +0100

    Day 133

---
+++ Day 122/Homework/Homework.py
@@ -0,0 +1,3 @@
+#1)The count function in python returns the number of entrys in a list
+#2)The fuction sorted returns a sorted order of the created list
+#3)the rest was codewars
--- Day123/Classwork/Classwork.py
+++
@@ -1,9 +0,0 @@
-#1) Create a list and add some names. Use the sorted() function to sort the names by the first letter of each name (alphabetically).
-
-names = ["Vika","Mari","Nino","andria"]
-print(sorted(names))
-
-#Create a list of numbers and add the numbers in descending order, your goal will be to sort these numbers in ascending order.
-
-Num = [3,5,1,9,5]
-print(sorted(Num))[::-1]
