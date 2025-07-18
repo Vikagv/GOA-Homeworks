48792c95b3462a949c81eea4d8b244435d6ce3c8
Mariam Gvilia <mariamgvilia@Mariams-MacBook-Air.local>
2025-07-18 16:05:54 +0400

    Day 150

---
+++ Day 150/Classwork/Classwork.js
@@ -0,0 +1,9 @@
+//create three variables to store the numbers. And in the console output the sum, multiplication, division, subtraction of these three numbers.
+
+let value1 = 67
+let value2 = 4
+let value3 = 9
+
+console.log(value1 + value2 + value3)
+console.log(value1 / value2 /value3)
+console.log(value1* value2 * value3)
---
+++ Day 150/Homework/Homework.js
@@ -0,0 +1,30 @@
+//Enter two numbers for the user and perform all actions. (+, -, *, /, **, % ) and display the result in alert like this. 'sum is: {sum}'
+
+Num = prompt ("Please enter a number")
+Num1 = prompt ("Please enter a second number")
+
+let sum1 = Num1 + Num2
+console.log(sum1)
+
+let sum2 = Num1 - Num2
+console.log(sum2)
+
+let sum3 = Num1 * Num2
+console.log(sum3)
+
+let sum4 = Num1 / Num2
+console.log(sum4)
+
+let sum5 = Num1 ** Num2
+console.log(sum5)
+
+let sum6 = Num1 % Num2
+console.log(sum6)
+
+alert(sum1+sum2+sum3+sum4+sum5+sum6)
+
+
+
+
+
+
