8e29eb3374097499cac2cd31751a1107b1f6f9a4
Mariam Gvilia <mariamgvilia@Mariams-MacBook-Air.local>
2025-08-16 16:23:01 +0400

    Day 158

---
+++ Day 158/Homework/Homework6.js
@@ -0,0 +1,8 @@
+//Ask the user to enter a number, execute the for loop from 1 to that number, and find the sum of all numbers in that range
+
+let usernum = Number(prompt("Enter a Number:"))
+
+for(let sum = 0; sum < usernum; sum ++){
+    console.log(sum)
+}
+
