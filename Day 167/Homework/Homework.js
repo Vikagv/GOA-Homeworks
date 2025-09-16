96b416d9222b071aa07ac8360ecfa7b274e2d1da
Mariam Gvilia <mariamgvilia@Mariams-MacBook-Air.local>
2025-09-16 20:32:33 +0100

    Day 167

---
+++ Day 167/Homework/Homework.js
@@ -0,0 +1,9 @@
+//Create a function that takes two numbers as arguments and returns their sum.
+
+function number(numbers){
+    let sum = Num1 + Num2 
+}
+let Num1 = Number(prompt("Enter a number: "))
+let Num2 = Number(prompt("Enter a second number: "))
+
+console.log(numbers)
